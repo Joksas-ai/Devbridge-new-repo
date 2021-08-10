127e9b42ebd15d87781493340b13a5ad0e4440f4
Joksas-ai <jokubas.narijauskas@gmail.com>
2021-08-11 01:57:45 +0300

    aha

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -70,6 +70,17 @@ test('If calculator multiplies correctly on prototype build ', async ({ page }) 
   expect(rezultatasTextContent).toBe('26');
 });
 
+test('If calculator multiplies correctly on 1 build ', async ({ page }) => {
+  await page.goto('https://testsheepnz.github.io/BasicCalculator');
+  await page.selectOption('#selectBuild', '1') /* Skaiciu keisti nuo 0 iki 10 */
+  await page.fill('#number1Field', '13');
+  await page.fill('#number2Field', '2');
+  await page.selectOption('#selectOperationDropdown', '2');
+  await page.click('#calculateButton');
+  const rezultatasTextContent = await page.inputValue('#numberAnswerField');
+  expect(rezultatasTextContent).toBe('26');
+});
+
 test('If calculator divides correctry on protype build', async ({ page }) => {
   await page.goto('https://testsheepnz.github.io/BasicCalculator');
   await page.selectOption('#selectBuild', '0') /* Skaiciu keisti nuo 0 iki 10 */
@@ -81,9 +92,31 @@ test('If calculator divides correctry on protype build', async ({ page }) => {
   expect(rezultatasTextContent).toBe('6.5');
 });
 
+test('If calculator divides correctry on 1 build', async ({ page }) => {
+  await page.goto('https://testsheepnz.github.io/BasicCalculator');
+  await page.selectOption('#selectBuild', '1') /* Skaiciu keisti nuo 0 iki 10 */
+  await page.fill('#number1Field', '13');
+  await page.fill('#number2Field', '2');
+  await page.selectOption('#selectOperationDropdown', '3');
+  await page.click('#calculateButton');
+  const rezultatasTextContent = await page.inputValue('#numberAnswerField');
+  expect(rezultatasTextContent).toBe('6.5');
+});
+
 test('If calculator concatenates correctly on protype build', async ({ page }) => {
   await page.goto('https://testsheepnz.github.io/BasicCalculator');
   await page.selectOption('#selectBuild', '0') /* Skaiciu keisti nuo 0 iki 10 */
+  await page.fill('#number1Field', '13');
+  await page.fill('#number2Field', '2');
+  await page.selectOption('#selectOperationDropdown', '4');
+  await page.click('#calculateButton');
+  const rezultatasTextContent = await page.inputValue('#numberAnswerField');
+  expect(rezultatasTextContent).toBe('132');
+});
+
+test('If calculator concatenates correctly on 1 build', async ({ page }) => {
+  await page.goto('https://testsheepnz.github.io/BasicCalculator');
+  await page.selectOption('#selectBuild', '1') /* Skaiciu keisti nuo 0 iki 10 */
   await page.fill('#number1Field', '13');
   await page.fill('#number2Field', '2');
   await page.selectOption('#selectOperationDropdown', '4');
